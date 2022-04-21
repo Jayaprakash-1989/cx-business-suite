782a1c452775523338cb64da3af42f7ec3b1ed6d
Jayaprakash-1989 <103124805+Jayaprakash-1989@users.noreply.github.com>
2022-04-21 09:36:56 +0530

    Creating project

---
+++ src/main/java/com/cxbusiness/suite/cx/CxBusinessSuiteApplication.java
@@ -0,0 +1,14 @@
+package com.cxbusiness.suite.cx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CxBusinessSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxBusinessSuiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cxbusiness/suite/cxbusinesssuite/CxBusinessSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxbusiness.suite.cxbusinesssuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxBusinessSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
